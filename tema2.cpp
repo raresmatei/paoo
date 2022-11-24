327cd408e527aa0c3604e4eaff5627c5f898556a
Rares Matei <rares.matei@doctarigroup.com>
2022-11-24 22:02:46 +0200

    finalized details

--- tema2.cpp
+++ tema2.cpp
@@ -11,46 +11,57 @@ class Job{
             title = titleVal;
             salary = salaryVal;
         }
+
+        void printJob(){
+            cout<<title<<"  "<<salary;
+        }
 };
 
 class BaseEmployee{
     private:
         string type;
     public:
-    BaseEmployee(string typeVal){
-        type = typeVal;
-    }
-    BaseEmployee(const BaseEmployee& rhs)
-    : type(rhs.type)
-    {
-        cout<<"base copy constructor"<<endl;
-    }
-    BaseEmployee& operator=(const BaseEmployee& rhs)
-    {
-        cout<<"base copy assignment operator"<<endl;
-        type = rhs.type; 
-        return *this; 
-    }
+        string getType(){
+            return type;
+        }
+        BaseEmployee(string typeVal){
+            type = typeVal;
+        }
+        BaseEmployee(const BaseEmployee& rhs)
+        : type(rhs.type)
+        {
+            cout<<"base copy constructor"<<endl;
+        }
+        BaseEmployee& operator=(const BaseEmployee& rhs)
+        {
+            cout<<"base copy assignment operator"<<endl;
+            type = rhs.type; 
+            return *this; 
+        }
+
 };
 
-class Employee: BaseEmployee{
+class Employee: public BaseEmployee{
     private:
         string name;
         int age;
         Job job;
     public:
-        Employee(string nameVal, int ageVal, string titleVal, int salaryVal): job(titleVal, salaryVal), BaseEmployee("full-time"){
+        Employee(string type, string nameVal, int ageVal, string titleVal, int salaryVal): BaseEmployee(type), job(titleVal, salaryVal){
             name = nameVal;
             age = ageVal;            
         }
         void swapFields(Employee& rhs){
             swap(job, rhs.job);
+            age = rhs.age;
+            name = rhs.name;
         }
 
         Employee& operator=(const Employee& rhs){
             cout<<"assignment operator"<<endl;
             BaseEmployee::operator=(rhs);
             Employee temp(rhs); 
+            cout<<"temp name: "<<temp.getName()<<endl;
             swapFields(temp);
             return *this;
         }
@@ -58,16 +69,34 @@ class Employee: BaseEmployee{
         Employee(const Employee& rhs)
         : BaseEmployee(rhs),
          job(rhs.job)
-        {
+        {          
+            name = rhs.name;
+            age = rhs.age;  
             cout<<"Employee copy constructor"<<endl;
         }
-        };
+
+        string getName(){
+            return name;
+        }
+
+        int getAge(){
+            return age;
+        }
+
+        Job getJob(){
+            return job;
+        }
+
+
+};
 
 int main(){
-    Employee e1("ana", 25, "eng", 2000);
-    Employee e2("rares", 21, "it", 3000);
+    Employee e1("part-time", "ana", 25, "eng", 2000);
+    Employee e2("full-time","rares", 21, "it", 3000);
 
     e1 = e2;
-    cout<<"--------------------"<<endl;
-    Employee e3(e2);
+
+    cout<<"e1: "<<e1.getType()<<" "<<e1.getName()<<" "<<e1.getAge()<<" ";
+    e1.getJob().printJob();
+
 }
